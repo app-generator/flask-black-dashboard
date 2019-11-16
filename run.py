bd252a4917b3bb981ba0e695b049a91cb4c1ad9e
app-generator <app-generator@appseed.us>
2019-11-16 18:02:31 +0200

    Code Formatting

--- run.py
+++ run.py
@@ -1,22 +1,7 @@
 # -*- encoding: utf-8 -*-
 """
-Flask Boilerplate
-Author: AppSeed.us - App Generator 
+License: MIT
+Copyright (c) 2019 - present AppSeed.us
 """
 
-import os
-from app import app
-from app import db
-
-#----------------------------------------
-# launch
-#----------------------------------------
-
-if __name__ == "__main__":
-	db.create_all()
-
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port, debug=True)
-	#app.run(ssl_context='adhoc')
-
-    
+from app import app, db
