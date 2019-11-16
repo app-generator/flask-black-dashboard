f667e472a265e5b93d9637805dc612c92b472f1f
app-generator <app-generator@appseed.us>
2019-11-16 18:01:16 +0200

    Code Formatting

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
 # -*- encoding: utf-8 -*-
 """
-Flask Boilerplate
-Author: AppSeed.us - App Generator 
+License: MIT
+Copyright (c) 2019 - present AppSeed.us
 """
 
-from flask_wtf          import FlaskForm, RecaptchaField
+from flask_wtf          import FlaskForm
 from flask_wtf.file     import FileField, FileRequired
 from wtforms            import StringField, TextAreaField, SubmitField, PasswordField
 from wtforms.validators import InputRequired, Email, DataRequired
@@ -14,7 +14,7 @@ class LoginForm(FlaskForm):
 	password    = PasswordField(u'Password'        , validators=[DataRequired()])
 
 class RegisterForm(FlaskForm):
+	name        = StringField  (u'Name'      )
 	username    = StringField  (u'Username'  , validators=[DataRequired()])
 	password    = PasswordField(u'Password'  , validators=[DataRequired()])
 	email       = StringField  (u'Email'     , validators=[DataRequired(), Email()])
-	name        = StringField  (u'Name'      , validators=[DataRequired()])
