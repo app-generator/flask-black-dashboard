cb474a50b0fefbb6410ff415d713dfb942d4cfaf
app-generator <app-generator@appseed.us>
2019-11-16 18:02:14 +0200

    Code Formatting

--- app/views.py
+++ app/views.py
@@ -1,50 +1,46 @@
 # -*- encoding: utf-8 -*-
 """
-Dark Dashboard - coded in Flask
-Author: AppSeed.us - App Generator 
+License: MIT
+Copyright (c) 2019 - present AppSeed.us
 """
 
-# all the imports necessary
-from flask import json, url_for, redirect, render_template, flash, g, session, jsonify, request, send_from_directory
+# Python modules
+import os, logging 
+
+# Flask modules
+from flask               import render_template, request, url_for, redirect, send_from_directory
+from flask_login         import login_user, logout_user, current_user, login_required
 from werkzeug.exceptions import HTTPException, NotFound, abort
 
-import os
+# App modules
+from app        import app, lm, db, bc
+from app.models import User
+from app.forms  import LoginForm, RegisterForm
 
-from app  import app
-
-from flask       import url_for, redirect, render_template, flash, g, session, jsonify, request, send_from_directory
-from flask_login import login_user, logout_user, current_user, login_required
-from app         import app, lm, db, bc
-from . models    import User
-from . common    import COMMON, STATUS
-from . assets    import *
-from . forms     import LoginForm, RegisterForm
-
-import os, shutil, re, cgi
-        
 # provide login manager with load_user callback
 @lm.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# authenticate user
+# Logout user
 @app.route('/logout.html')
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
-# register user
+# Register a new user
 @app.route('/register.html', methods=['GET', 'POST'])
 def register():
     
-    # define login form here
+    # declare the Registration Form
     form = RegisterForm(request.form)
 
     msg = None
 
-    # custommize your pate title / description here
-    page_title       = 'Register - Flask Dark Dashboard | AppSeed App Generator'
-    page_description = 'Open-Source Flask Dark Dashboard, registration page.'
+    if request.method == 'GET': 
+
+        return render_template('layouts/default.html',
+                                content=render_template( 'pages/register.html', form=form, msg=msg ) )
 
     # check if both http method is POST and form is valid on submit
     if form.validate_on_submit():
@@ -52,7 +48,6 @@ def register():
         # assign form data to variables
         username = request.form.get('username', '', type=str)
         password = request.form.get('password', '', type=str) 
-        name     = request.form.get('name'    , '', type=str) 
         email    = request.form.get('email'   , '', type=str) 
 
         # filter User out of database through username
@@ -64,35 +59,31 @@ def register():
         if user or user_by_email:
             msg = 'Error: User exists!'
         
-        else:                    
-            pw_hash = bc.generate_password_hash(password)
+        else:         
 
-            user = User(username, pw_hash, name, email)
+            pw_hash = password #bc.generate_password_hash(password)
+
+            user = User(username, email, pw_hash)
 
             user.save()
 
             msg = 'User created, please <a href="' + url_for('login') + '">login</a>'     
 
-    # try to match the pages defined in -> themes/light-bootstrap/pages/
-    return render_template( 'layouts/default.html',
-                            title=page_title,
-                            content=render_template( 'pages/register.html', 
-                                                     form=form,
-                                                     msg=msg) )
+    else:
+        msg = 'Input error'     
 
-# authenticate user
+    return render_template('layouts/default.html',
+                            content=render_template( 'pages/register.html', form=form, msg=msg ) )
+
+# Authenticate user
 @app.route('/login.html', methods=['GET', 'POST'])
 def login():
     
-    # define login form here
+    # Declare the login form
     form = LoginForm(request.form)
 
     # Flask message injected into the page, in case of any errors
     msg = None
-
-    # custommize your page title / description here
-    page_title = 'Login - Flask Dark Dashboard | AppSeed App Generator'
-    page_description = 'Open-Source Flask Dark Dashboard, login page.'
 
     # check if both http method is POST and form is valid on submit
     if form.validate_on_submit():
@@ -106,7 +97,8 @@ def login():
 
         if user:
             
-            if bc.check_password_hash(user.password, password):
+            #if bc.check_password_hash(user.password, password):
+            if user.password == password:
                 login_user(user)
                 return redirect(url_for('index'))
             else:
@@ -114,143 +106,30 @@ def login():
         else:
             msg = "Unkkown user"
 
-    # try to match the pages defined in -> themes/light-bootstrap/pages/
-    return render_template( 'layouts/default.html',
-                            title=page_title,
-                            content=render_template( 'pages/login.html', 
-                                                     form=form,
-                                                     msg=msg) )
-
-# Used only for static export
-@app.route('/user.html')
-def user():
-
-    # custommize your page title / description here
-    page_title = 'Icons - Flask Dark Dashboard | AppSeed App Generator'
-    page_description = 'Open-Source Flask Dark Dashboard, the icons page.'
-
-    # try to match the pages defined in -> pages/
     return render_template('layouts/default.html',
-                            content=render_template( 'pages/user.html') )
-
-# Used only for static export
-@app.route('/icons.html')
-def icons():
-
-    # custommize your page title / description here
-    page_title = 'Icons - Flask Dark Dashboard | AppSeed App Generator'
-    page_description = 'Open-Source Flask Dark Dashboard, the icons page.'
-
-    # try to match the pages defined in -> pages/
-    return render_template('layouts/default.html',
-                            content=render_template( 'pages/icons.html') )
-
-# Used only for static export
-@app.route('/tables.html')
-def tables():
-
-    # custommize your page title / description here
-    page_title = 'Tables - Flask Dark Dashboard | AppSeed App Generator'
-    page_description = 'Open-Source Flask Dark Dashboard, the tables page.'
-
-    # try to match the pages defined in -> pages/
-    return render_template('layouts/default.html',
-                            content=render_template( 'pages/tables.html') )
-
-# Used only for static export
-@app.route('/notifications.html')
-def notifications():
-
-    # custommize your page title / description here
-    page_title = 'Tables - Flask Dark Dashboard | AppSeed App Generator'
-    page_description = 'Open-Source Flask Dark Dashboard, the tables page.'
-
-    # try to match the pages defined in -> pages/
-    return render_template('layouts/default.html',
-                            content=render_template( 'pages/notifications.html') )
-
-# Used only for static export
-@app.route('/typography.html')
-def typography():
-
-    # custommize your page title / description here
-    page_title = 'Typography - Flask Dark Dashboard | AppSeed App Generator'
-    page_description = 'Open-Source Flask Dark Dashboard, the tables page.'
-
-    # try to match the pages defined in -> pages/
-    return render_template('layouts/default.html',
-                            content=render_template( 'pages/typography.html') )
+                            content=render_template( 'pages/login.html', form=form, msg=msg ) )
 
 # App main route + generic routing
 @app.route('/', defaults={'path': 'index.html'})
 @app.route('/<path>')
 def index(path):
 
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+
     content = None
 
     try:
 
-        # try to match the pages defined in -> themes/light-bootstrap/pages/
+        # try to match the pages defined in -> pages/<input file>
         return render_template('layouts/default.html',
                                 content=render_template( 'pages/'+path) )
     except:
-        abort(404)
+        
+        return render_template('layouts/auth-default.html',
+                                content=render_template( 'pages/404.html' ) )
 
-#@app.route('/favicon.ico')
-#def favicon():
-#    return send_from_directory(os.path.join(app.root_path, 'static'),
-#                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-#@app.route('/sitemap.xml')
-#def sitemap():
-#    return send_from_directory(os.path.join(app.root_path, 'static'),
-#                               'sitemap.xml')
-
-# ------------------------------------------------------
-
-# error handling
-# most common error codes have been added for now
-# TO DO:
-# they could use some styling so they don't look so ugly
-
-def http_err(err_code):
-	
-    err_msg = 'Oups !! Some internal error ocurred. Thanks to contact support.'
-	
-    if 400 == err_code:
-        err_msg = "It seems like you are not allowed to access this link."
-
-    elif 404 == err_code:    
-        err_msg  = "The URL you were looking for does not seem to exist."
-        err_msg += "<br /> Define the new page in /pages"
-    
-    elif 500 == err_code:    
-        err_msg = "Internal error. Contact the manager about this."
-
-    else:
-        err_msg = "Forbidden access."
-
-    return err_msg
-    
-@app.errorhandler(401)
-def e401(e):
-    return http_err( 401) # "It seems like you are not allowed to access this link."
-
-@app.errorhandler(404)
-def e404(e):
-    return http_err( 404) # "The URL you were looking for does not seem to exist.<br><br>
-	                      # If you have typed the link manually, make sure you've spelled the link right."
-
-@app.errorhandler(500)
-def e500(e):
-    return http_err( 500) # "Internal error. Contact the manager about this."
-
-@app.errorhandler(403)
-def e403(e):
-    return http_err( 403 ) # "Forbidden access."
-
-@app.errorhandler(410)
-def e410(e):
-    return http_err( 410) # "The content you were looking for has been deleted."
-
-	
+# Return sitemap 
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'sitemap.xml')
