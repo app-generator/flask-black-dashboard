7f5b91af4a4a5e8a35e559d915dab2da499735fe
App Generator <51070104+app-generator@users.noreply.github.com>
2022-09-19 18:47:41 +0300

    Integrate flask_minify (page compression)

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 
 import os
 from   flask_migrate import Migrate
+from   flask_minify  import Minify
 from   sys import exit
 
 from apps.config import config_dict
@@ -27,6 +28,9 @@ except KeyError:
 app = create_app(app_config)
 Migrate(app, db)
 
+if not DEBUG:
+    Minify(app=app, html=True, js=False, cssless=False)
+    
 if DEBUG:
     app.logger.info('DEBUG       = ' + str(DEBUG)             )
     app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI)
