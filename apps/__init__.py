2bd0f9d81f3968bf8d8d55c9f9ebe6b0342bbf01
Buddha23-RGB <53986719+Buddha23-RGB@users.noreply.github.com>
2024-11-29 12:35:50 -0700

    Co-authored-by: Buddha23-RGB <joechmoe555@hotmail.com>

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,23 +1,27 @@
 # -*- encoding: utf-8 -*-
 
+from apps.authentication.oauth import github_blueprint
 import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from importlib import import_module
-from config import Config
+from apps.config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+
 
 def register_extensions(app):
     db.init_app(app)
     login_manager.init_app(app)
 
+
 def register_blueprints(app):
     for module_name in ('authentication', 'home'):
         module = import_module('apps.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
+
 
 def configure_database(app):
     @app.before_first_request
@@ -28,7 +32,8 @@ def configure_database(app):
             print('> Error: DBMS Exception: ' + str(e))
             # fallback to SQLite
             basedir = os.path.abspath(os.path.dirname(__file__))
-            app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+            app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+                os.path.join(basedir, 'db.sqlite3')
             print('> Fallback to SQLite ')
             db.create_all()
 
@@ -36,7 +41,6 @@ def configure_database(app):
     def shutdown_session(exception=None):
         db.session.remove()
 
-from apps.authentication.oauth import github_blueprint
 
 def create_app(config_class=Config):
     app = Flask(__name__)
